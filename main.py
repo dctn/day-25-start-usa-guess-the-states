4a5cdf479a0c53b304fb6f80fbade9b47d1d1a34
stranger <dctngamers@gmail.com>
2024-05-03 21:29:12 +0530

    GUESS THE U.S.A STATES PROJECTS FINISHED

--- main.py
+++ main.py
@@ -37,39 +37,32 @@ def state_position(user_input):
 
 
 score = 0
-
-
-# def score_func(score_parameter):
-#     score_parameter += 1
-    # return score
-#
-
 # looping program
 game_on = True
+guessed_states = []
+unguessed_states = []
 while game_on:
 
     user_input_raw = screen.textinput(title="guess the states", prompt=f"you guessed {score}/50 states")
     user_input = str(user_input_raw).capitalize()
 
+    if user_input == "Exit":
+        game_on = False
+        for i in states_name_list:
+            if i not in guessed_states:
+                unguessed_states.append(i)
+
+        unguessed_states_dict = {
+            "unguessed states": unguessed_states
+        }
+        unguessed_states_data = pandas.DataFrame(unguessed_states_dict)
+        unguessed_states_data.to_csv("unguessed states.csv")
+
+
+
     if user_input in states_name_list:
+        guessed_states.append(user_input)
         state_position(user_input)
         score += 1
 
-        # score_func(score)
 
-        # guess_row = data[data.state == user_input]
-        # x_cor_raw = guess_row.x
-        # x_cor_list = x_cor_raw.to_list()
-        # x_cor = x_cor_list[0]
-        #
-        # y_cor_raw = guess_row.y
-        # y_cor_list = y_cor_raw.to_list()
-        # y_cor = y_cor_list[0]
-        #
-        # name = t.Turtle()
-        # name.penup()
-        # name.goto(x=x_cor, y=y_cor)
-        # name.hideturtle()
-        # name.write(arg=user_input, align="center", font=("Arial", 8, "normal"))
-
-screen.exitonclick()
